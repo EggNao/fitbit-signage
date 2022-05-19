e1f4611e1c3592961b40d6234ebd5fe8db49bcc2
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-05-19 12:44:48 +0900

    fix: チャートに対してラベルを帰る

--- front/src/components/lineChartCard/index.tsx
+++ front/src/components/lineChartCard/index.tsx
@@ -11,7 +11,7 @@ export type LineChartCardProps = {
 export const LineChartCard: React.VFC<LineChartCardProps> = ({ dataArray, dateArray, scoreType, color, text }) => {
   return (
     <div className='border rounded drop-shadow-md p-10 m-2'>
-      <span className='text-3xl'>{text}</span>
+      <span className='text-3xl'>{scoreType}</span>
       <LineChart dataArray={dataArray} dateArray={dateArray} scoreType={scoreType} color={color} />
     </div>
   )
