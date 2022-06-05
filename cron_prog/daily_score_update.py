c323e1e346d21d6712ea51e2032dc0ab0176b74d
i16fujimoto <fujimoto.ryusei@arakawa-lab.com>
2022-06-06 03:15:50 +0900

    get cron prog

--- cron_prog/daily_score_update.py
+++ cron_prog/daily_score_update.py
@@ -2,9 +2,9 @@ import requests
 
 try:
     url="http://localhost:8000/fitbit"
-    r = requests.post(url+"/")
-    #if(r.form['ret']=="ok"):#きちんと送信できたか確認できるように！
-    print("susess")
+    r = requests.get(url+"/9YHK8W").json()
+    print("fitbit request success")
+    print(r)
 
 except Exception as e:
     print(e)
